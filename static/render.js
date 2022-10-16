61901e1396f44f9bf3016724ad5410b4fe5a5ebe
Anshuman Kumar <anshumankumar2101@gmail.com>
2022-10-16 10:47:52 +0530

    first commit

--- static/render.js
+++ static/render.js
@@ -6,7 +6,7 @@ window.onload = function () {
             console.log(window);
             var opt = {
                 margin: 1,
-                orientation:
+                
                 filename: 'myfile.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
